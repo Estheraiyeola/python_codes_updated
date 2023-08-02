1fd1fbb598fea67f83d1a4b9c8ea80a8dd552e4f
Estheraiyeola <estheraiyeola@yahoo.com>
2023-08-02 22:21:13 +0100

    Removes duplicate element in a set

---
+++ removes_duplicates.py
@@ -0,0 +1,14 @@
+def removes_duplicate(arr):
+    new_arr = []
+    for index in arr:
+        if index in new_arr:
+            continue
+        else:
+            new_arr += [index]
+    return new_arr
+
+
+array = [1, 2, 3, 3, 4, 5, 1, 3, 2]
+string = ["esther", "seyi", "esther"]
+
+print(removes_duplicate(string))
